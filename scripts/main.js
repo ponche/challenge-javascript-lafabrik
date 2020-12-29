b5ebc8bd8a31433505a5ef24560ecf2029d579a9
Rémi <ponche62880@gmail.com>
2020-12-30 00:52:36 +0100

    problem with axios

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,26 @@
+
+
 /**
  * @todo Point d'entrée pour faire vos exercices...
  */
 window.onload = init
 
 function init() {
-    console.log("Coucou ! C'est ici que l'aventure commence ! Bon courage :-)")
+     axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'; 
+     axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*'; 
+    // console.log("Coucou ! C'est ici que l'aventure commence ! Bon courage :-)")
+    // const API = new LibraryApi('https://api.jikan.moe/v3'); 
+    // console.log(API.fetch('/'))
+
+    // test desepéré 
+    fetch('https://api.jikan.moe/v3').then(function(response){
+        console.log('avec un fetch classique'); 
+        console.log(response); 
+    });
+
+    const API = new LibraryApi('https://api.kikan.moe/v3'); 
+    API.fetch('/').then(function(response){
+        console.log('avec Axios'); 
+        console.log(response); 
+    })
 }
